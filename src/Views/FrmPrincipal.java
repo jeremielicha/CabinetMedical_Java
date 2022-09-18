2a8fc227aeb5f2fce1100b718e14d3df81ecb603
jeremielicha <jeremielicha7@gmail.com>
2022-09-18 18:27:39 +0200

    ajout des dates et des heures dans les treemap

--- src/Views/FrmPrincipal.java
+++ src/Views/FrmPrincipal.java
@@ -8,6 +8,9 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.TreeMap;
 
 public class FrmPrincipal extends JFrame {
@@ -27,9 +30,10 @@ public class FrmPrincipal extends JFrame {
     private JLabel lblDateRdv;
     private JTree trRdv;
     private JDateChooser cldDate;
-    private TreeMap<String,TreeMap<String,RendezVous>>monPlanning;
+    private TreeMap<String,TreeMap<String,RendezVous>>tmMonPlanning;
     DefaultMutableTreeNode root;
     DefaultTreeModel model;
+    RendezVous rendezVous;
     public FrmPrincipal() {
         this.setTitle("Cabinet Medical");
         this.setContentPane(pnlRoot);
@@ -41,8 +45,9 @@ public class FrmPrincipal extends JFrame {
         cldDate=new JDateChooser();
         cldDate.setDateFormatString("dd/MM/yyyy");
         pnlDate.add(cldDate);
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
         spHeures.setModel(new SpinnerNumberModel(7,7,18,1));
-        spMinutes.setModel(new SpinnerNumberModel(00,00,45,15));
+        spMinutes.setModel(new SpinnerNumberModel(0, 0,45,15));
 
         root = new DefaultMutableTreeNode("Mon planning");
         model = new DefaultTreeModel(root);
@@ -53,27 +58,37 @@ public class FrmPrincipal extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                tmMonPlanning=new TreeMap<>();
+                TreeMap<String, RendezVous> tmHeure= new TreeMap<>();
+
+                String nomPatient=txtNomPatient.getText();
+                String nomPatologie=cbxPatologie.getSelectedItem().toString();
+                String date= sdf.format(cldDate.getDate());
+                String heure=spHeures.getValue().toString()+" : "+spMinutes.getValue().toString();
+
                 if (txtNomPatient.getText().isEmpty()){
                     JOptionPane.showMessageDialog(null,"Saisir le nom du patient");
                 } else if (cldDate.getDate()==null) {
                     JOptionPane.showMessageDialog(null,"Saisir une date");
                 }else {
-                    String nomPatient=txtNomPatient.getText();
-                    String nomPatologie=cbxPatologie.getSelectedItem().toString();
-                    String date=cldDate.getDate().toString();
-                    String heure=spHeures.getValue().toString();
-                    String minute=spMinutes.getValue().toString();
-
-                    RendezVous rendezVous=new RendezVous(heure,nomPatient,nomPatologie);
-
-                    TreeMap<String, RendezVous> tmHeure= new TreeMap<>();
-                    monPlanning=new TreeMap<>();
-
-                    tmHeure.put(heure,rendezVous);
-                    monPlanning.put(date,tmHeure);
-
-                    System.out.println("Planning : "+monPlanning);
-
+                    if (!tmMonPlanning.containsKey(date)){
+                        rendezVous=new RendezVous(heure,nomPatient,nomPatologie);
+                        tmHeure.put(heure,rendezVous);
+                        tmMonPlanning.put(date,tmHeure);
+                    }else {
+                        if (!tmMonPlanning.get(date).containsKey(heure)){
+                            RendezVous rendezVous=new RendezVous(heure,nomPatient,nomPatologie);
+                            tmMonPlanning.get(date).put(heure,rendezVous);
+                        }else {
+                            JOptionPane.showMessageDialog(null,"Rdv deja pris");
+                        }
+                    }
+                }
+                for(String keyDate: tmMonPlanning.keySet()){
+                    System.out.println(keyDate);
+                    for (String keyHeure: tmMonPlanning.get(keyDate).keySet()){
+                        System.out.println(keyHeure);
+                    }
                 }
             }
         });
